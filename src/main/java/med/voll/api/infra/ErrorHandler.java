13f459bc6e99d2e3b080f6835e364dc17988112e
Richard Calderan <richardcck@hotmail.com>
2025-11-09 11:49:00 -0300

    Refactoring to handle responses

---
+++ src/main/java/med/voll/api/infra/ErrorHandler.java
@@ -0,0 +1,34 @@
+package med.voll.api.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Void> HandleError404(){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<BadRequestData>> HandleBadRequest(MethodArgumentNotValidException exception){
+        var errors = exception.getFieldErrors();
+        return ResponseEntity.badRequest().body(
+                errors.stream()
+                        .map(BadRequestData::new).toList()
+        );
+    }
+
+    public record BadRequestData(String field, String message){
+        BadRequestData(FieldError fieldError){
+            this(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+    }
+}
