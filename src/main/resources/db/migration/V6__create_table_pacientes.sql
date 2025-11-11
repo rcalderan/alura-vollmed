c7265b44d8001608b6a17dbb2d9bd4b365d90553
Richard Calderan <richardcck@hotmail.com>
2025-11-11 07:59:14 -0300

    Update project to adapt next steps: create pacientes (with heritage from abstract class). Set secutity open to easy develop.

---
+++ src/main/resources/db/migration/V6__create_table_pacientes.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Pacientes (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    convenio VARCHAR(50) NOT NULL,
+
+    logradouro VARCHAR(100),
+    bairro VARCHAR(100),
+    cep VARCHAR(20),
+    cidade VARCHAR(100),
+    uf VARCHAR(2),
+    numero VARCHAR(20),
+    complemento VARCHAR(100)
+);
